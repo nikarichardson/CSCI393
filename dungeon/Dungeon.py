e91f3aaa9c528e8be2e78d9d89fb0064ab7e7b33
Nika <40258517+nikarichardson@users.noreply.github.com>
2019-04-07 18:58:19 -0700

    still has bug with format inventory (take item)

--- dungeon/Dungeon.py
+++ dungeon/Dungeon.py
@@ -12,6 +12,22 @@ class Dungeon:
 
     dungeon_map = "dungeon.map"
     prompt = '> '
+   
+    ## MONSTER IDEAS: minotaur, orc, plant, rat, ogre, scorpion, skeleton, slime, snake
+    ## succubus, werewolf, zombie, skeleton, vampire,
+    ## chimera, cerberus, spider, ghost, fairy, dragon,
+
+    ## LOOT IDEAS: plain-chest, golden-chest, steel-chest, mini-chest
+    ## mana-crystal, pick-axe, potion, book, tome, ring 
+
+    ## EQUIPMENT: (implement-later?) weapon, shield, head, body, accessory 
+
+    ## WEAPONS: sword, pick-axe, bow
+
+    ## CLASSES (implement-later?): hero, warrior, mage, priest
+
+    ## SKILLS (imeplement-later?): attack, guard, double-attack,triple-attack,heal
+
 
     def repl(self):
         cmd = ''
@@ -78,17 +94,18 @@ class Dungeon:
                 # / wielding a pickaxe or dynamite or something like that
                 descs = line.split("|")
                 words = descs[0].split()
-                if len(words) < 3 or len(descs) != 3:
-                    print("usage: dig <direction> <reverse> | <brief description of new room> | <florid description of new room>")
+                if len(words) < 3 or len(descs) != 4:
+                    print("usage: dig <direction> <reverse> | <brief description of new room> | <florid description of new room> | <loot item>")
                     continue
                 forward = words[1]
                 reverse = words[2]
                 brief = descs[1].strip() # strip removes whitespace around |'s
                 florid = descs[2].strip()
+                loot = descs[3].strip()
                 # now that we have the directions and descriptions,
                 # add the new room, and stitch it in to the dungeon
                 # via its exits
-                query = 'INSERT INTO rooms (short_desc, florid_desc, visit) VALUES ("{}", "{}","{}")'.format(brief, florid,0)
+                query = 'INSERT INTO rooms (short_desc, florid_desc,visit,loot) VALUES ("{}", "{}","{}","{}")'.format(brief,florid,0,loot)
                 # print (query)
                 self.c.execute(query)
                 new_room_id = self.c.lastrowid
@@ -99,14 +116,65 @@ class Dungeon:
                 self.c.execute(query)
 
             elif words[0] == 'spawn':
-                # todo: spawn a monster object
-                continue
+                # spawn a monster object
+                print("You can spawn the following monster objects: minotaur, orc, plant, rat, ogre, scorpion, skeleton, slime, snake, succubus, werewolf, zombie, skeleton, vampire, chimera, cerberus, spider, ghost, fairy, dragon.")
+                my_monster = str(input("Choose a monster object."))
+   
+                ## now we need to update the current room with the monster object 
+                self.c.execute("UPDATE rooms SET visit = 1 WHERE id={}".format(self.current_room))
+                self.c.execute("INSERT INTO mobs (name,health,room_id) VALUES ({},100,{}".format(my_monster,self.current_room))
+                print("You've spawned a {}.".format(answer))
 
             elif words[0] == 'take':
-                # todo: track the player's inventory
-                # todo: create a command to place loot
-                # todo: implement take
+                # we only allow one object per room 
+                # get the name of the loot available in this room 
+                self.c.execute("SELECT loot FROM rooms WHERE id={}".format(self.current_room))
+                item = str(self.c.fetchone()[0]) 
+
+                print("Are you sure you want to take {}?".format(item)) 
+                answer = int(input("Press 1 to confirm."))
+             
+                if answer == 1:
+                    # remove loot from the room (update table)
+                    self.c.execute("UPDATE rooms SET loot = 'none' WHERE id={}".format(self.current_room))
+
+                    # insert item into inventory 
+                    self.c.execute("INSERT INTO inventory (name) VALUES ({}".format(item))
+                    print("You took {}!".format(item))
+                continue 
+
+                
+            elif words[0] == 'view':
+                # to do: implement view player stats 
                 continue
+
+            elif words[0] == 'check':
+                # check inventory for object 
+                self.c.execute("SELECT name FROM inventory") 
+                count = 0 
+
+                for item in self.c.fetchall():
+                    print("You have in your inventory: ",end='')
+                    print("{}".format(item[0]), end='')
+                    count = count + 1 
+
+                if count == 0:
+                    print("There's nothing in your inventory, you poor penniless pauper!", end='')
+                
+                print("")
+
+            elif words[0] == 'place':
+                # place loot command 
+                answer = str(input("Choose an item from your inventory."))
+                # update the room with the chosen loot 
+                self.c.execute("UPDATE rooms SET loot = answer WHERE id={}".format(self.current_room))
+
+                print("You've placed {} in the room.".format(answer))
+                
+                ## remove this item from our inventory now 
+                ## right now we are assuming that user will only ask to place an item
+                ## that is actually in their inventory 
+                self.c.execute("DELETE FROM inventory WHERE name={}".format(answer))
 
             # todo: if player ends turn in a room with a hostile mob
             # figure out how to handle combat.
@@ -132,9 +200,18 @@ class Dungeon:
             status = self.c.fetchone()[0]
 
             if status == 0:        
-                ## we have not visited this room yet—give florid descrition
+                ## we have not visited this room yet—give florid description
                 self.c.execute("SELECT florid_desc FROM rooms WHERE id={}".format(self.current_room))
                 print(self.c.fetchone()[0])
+
+                ## read off the available loot
+                self.c.execute("SELECT loot FROM rooms WHERE id={}".format(self.current_room))
+                item = str(self.c.fetchone()[0])
+
+                if item != 'none':
+                    print("This room contains a {}.".format(item)) 
+                else:
+                    print("Nothing is in this room.") 
 
                 ## update visit integer mark this room as visited
                 self.c.execute("UPDATE rooms SET visit = 1 WHERE id={}".format(self.current_room))
@@ -143,6 +220,15 @@ class Dungeon:
                 ## we've visited this room already—give simple description
                 self.c.execute("SELECT short_desc FROM rooms WHERE id={}".format(self.current_room))
                 print(self.c.fetchone()[0])
+
+                ## read off the available loot
+                self.c.execute("SELECT loot FROM rooms WHERE id={}".format(self.current_room))
+                item = str(self.c.fetchone()[0]) 
+
+                if item != 'none':
+                    print("This room contains a {}.".format(item)) 
+                else:
+                    print("Nothing is in this room.") 
 
 
         ## Give the full-description since force-florid is switched on. 
@@ -155,7 +241,7 @@ class Dungeon:
         print("There are exits in these directions: ", end='')
         count = 0 
         for exit in self.c.fetchall():
-            print("{}  ".format(exit[0]), end='')
+            print("{}".format(exit[0]), end='')
             count = count + 1 
         if count == 0:
             print("none found", end='')
@@ -170,14 +256,18 @@ class Dungeon:
         if (db_exists == None):
             self.c.execute("DROP TABLE if exists rooms")
             self.c.execute("DROP TABLE if exists mobs")
+            self.c.execute("DROP TABLE if exists inventory")
             self.c.execute("DROP TABLE if exists loot")
             self.c.execute("DROP TABLE if exists exits")
-            self.c.execute("CREATE TABLE rooms (id INTEGER PRIMARY KEY AUTOINCREMENT, short_desc TEXT, florid_desc TEXT, visit INTEGER)")
+            ## API: rooms will keep track of the name of the loot item that they contain, if any 
+            self.c.execute("CREATE TABLE rooms (id INTEGER PRIMARY KEY AUTOINCREMENT, short_desc TEXT, florid_desc TEXT, visit INTEGER, loot TEXT)")
             self.c.execute("CREATE TABLE mobs (id INTEGER PRIMARY KEY AUTOINCREMENT, desc TEXT, health INTEGER, room_id INTEGER)")
             self.c.execute("CREATE TABLE exits (from_room INTEGER, to_room INTEGER, dir TEXT)")
-            # todo: create table for loot items
-            # todo: create table for "held_by" relationship ?? 
-            self.c.execute("INSERT INTO rooms (florid_desc, short_desc,visit) VALUES ('You are standing at the entrance of what appears to be a vast, complex cave.', 'entrance',0)")
+            # table for loot items
+            self.c.execute("CREATE TABLE loot (id INTEGER PRIMARY KEY AUTOINCREMENT, name TEXT, des TEXT, available INTEGER)")
+            # table for inventory 
+            self.c.execute("CREATE TABLE inventory (name TEXT)")
+            self.c.execute("INSERT INTO rooms (florid_desc, short_desc,visit,loot) VALUES ('You are standing at the entrance of what appears to be a vast, complex cave.', 'entrance',0,'none')")
             self.db.commit()
 
         # now we know the db exists - fetch the first room, which is
@@ -209,4 +299,5 @@ if __name__ == '__main__':
 
     print("Welcome to the dungeon ( ͡° ͜ʖ ͡°) Try 'look' to see room descriptions, 'go' to use an exit,")
     print("'dig' to create a new room, and 'new' to start the dungeon creation process over again.")
+    print("Use 'check' to survey your inventory, 'take' to steal loot, and 'place' to leave loot behind.")
     d.repl()
