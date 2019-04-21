6c1b8d2ee26a5e0d97a4c391c5c4ba8aaedd88ce
nikarichardson <nikarichardson@yahoo.com>
2019-04-21 14:44:54 -0700

    create erebor

---
+++ dungeon/erebor.py
@@ -0,0 +1,80 @@
+## To run this preformed dungeon type python3 erebor.py | python3 Dungeon.py into the terminal. 
+print("set_status:super")
+print("spawn")
+print("dinosaur-of-yore🦕")
+print("dig w e | crystal room | You are in a room that is glowing blue from the light of crystals scattered across the room. Be careful-those look sharp! | crystal")
+print("w") # inside crystal room
+print("spawn")
+print("dragon🐉")
+print("e") # back to the entrance
+print("dig s n | room of darkness | It's too dark to see much in here! | money-bag💰")
+print("s") # inside room of darkness 
+print("spawn")
+print("owl🦉")
+print("dig e w | plant room | This room is covered in plants! | herb🌿")
+print("e") # inside the plant room 
+print("spawn")
+print("mostly-friendly-wolf🐺")
+print("w") # back inside room of darkness 
+print("dig w e | water room | You are in a room with an underground lake. The water is dark and uninviting.| potion")
+print("dig s n | explosive room | It's hard to explain. This room is electric, it's on fire, it's explosively... oh, that's why! There's dynamite 💣 in this room. Best not pick that up, though. | shield ")
+print("s") # inside explosive room 
+print("spawn")
+print("jack-o-lantern🎃")
+print("dig e w | golden room | This room is glittering gold. | golden-chest")
+print("e") # inside golden room
+print("spawn")
+print("minotaur")
+print("w") # back inside explosive room
+print("dig s n | amusement park room | Looks like fun in here 🎢 🎡 | the-american-dream🏠")
+print("s") # inside amusement park room
+print("spawn")
+print("unicorn🦄")
+print("dig e w | fruit and zombie room | There are zombies eating fruit in this room. Bizarre. | cherry🍒")
+print("e") # inside fruit and zombie room
+print("spawn")
+print("zombie🧟")
+print("w") # back to amusement partk room 
+print("dig s n | room of New Age Philosophers 🧘| You see a group of New Age (armchair) philosophers meditating in lotus pose 🧘. It's doesn't seem like they are accomplishing much by meditating . . .  | mini-chest")
+print("s") # inside room of New Age Philosophers
+print("spawn")
+print("caterpillar-of-phenomenal-power🐛")
+print("dig s n | room of the perpetually lost | You don't even know where you are. | tent⛺️") 
+print("s") # inside room of the perpetually lost 
+print("spawn")
+print("umbrella🌂") 
+print("n")
+print("n") 
+print("n")
+print("n")
+print("n") # back to the room of darkness
+print("w")
+print("spawn") # inside water room 
+print("dolphin🐬")
+print("dig n s | room of loquacity 💬 | All of a sudden you have a strange urge to talk incessantly 💬 | apple🍎")
+print("n") # inside room of loquacity 
+print("spawn")
+print("kleptomaniac-squirrel-of-doom🐿")
+print("dig e w | sword room | You are in an unremarkable room. | sword ") 
+print("e") # inside sword room 
+print("spawn")  
+print("bee-of-disproportionate-size🐝")
+print("w")
+print("s") # back inside water room 
+print("dig s n | library | A library? In the middle of a dungeon? How excellent, how unlikely! | tome") 
+print("s") # inside library 
+print("spawn")
+print("the-great-mage🧙‍♂️")
+print("dig s n | circus room | It's a circus! Too bad it looks like the troupe is full. 🎪 | revival-dove🕊")
+print("s") # inside circus room
+print("spawn")
+print("magical-fish-out-of-water🐟")
+print("dig s n | outer space room | This room is simply out of this world. | ISS🛰")
+print("s") # inside the outer space room 
+print("spawn")
+print("alien🛸")
+print("dig w e | fountain room | Hey, there's a fountain here! ⛲ | orange-book📙 ")
+print("w") # inside fountain room
+print("spawn")
+print("octopus🐙")
+print("q")
