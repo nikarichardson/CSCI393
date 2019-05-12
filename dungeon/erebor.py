047f8e9a2dda8a1cf7a53280ef3ec46ecfa00553
Nika <40258517+nikarichardson@users.noreply.github.com>
2019-05-12 01:06:07 -0700

    19 rooms added

--- dungeon/erebor.py
+++ dungeon/erebor.py
@@ -39,10 +39,95 @@ print("dig s n | room of New Age Philosophers 🧘| You see a group of New Age (
 print("s") # inside room of New Age Philosophers
 print("spawn")
 print("caterpillar-of-phenomenal-power🐛")
-print("dig s n | room of the perpetually lost | You don't even know where you are. | tent⛺️") 
+print("dig s n | room of the perpetually lost | You do not even know where you are. | tent⛺️") 
 print("s") # inside room of the perpetually lost 
 print("spawn")
 print("umbrella🌂") 
+print("dig e w | room of dionysus | You are in a dark, mysterious room with many candles dimly flickering. | grapes🍇") 
+print("e") # inside the room of dionysus 
+print("spawn")
+print("bat🦇") 
+print("w") # back to room of perpetually lost
+print("dig s n | room of the page of swords | You are surrounded by decorative swords and golden framed paintings of ancient paladins on the walls. | dagger🗡") 
+print("s") # inside the room of the page of swords
+print("spawn")
+print("cerberus")
+print("dig e w | room of the unassailable | You have a feeling this room is unconquerable. And thus, epic. | watermelon🍉") 
+print("e") # inside room of the unassailable 
+print("spawn")
+print("time⏱")
+print("dig s n | room of the ancients | This room is old. Really old. | amphora-of-the-ancients🏺") 
+print("s") # inside room of the ancients
+print("spawn")
+print("ogre")
+print("dig s n | city of Athens | Wow! An entire city in one room. | Athens🏛") 
+print("s") # inside city of Athens
+print("spawn")
+print("bee-of-disproportionate-size🐝")
+print("n") # back inside room of the ancients
+print("n") # back inside room of the unassailable
+print("w") # back inside room of the page of swords 
+print("dig s n | room of slime | Hm. Slime. Everywhere. That is unpleasant. | mushroom🍄") 
+print("s") # inside room of slime
+print("spawn")
+print("slime")
+print("dig s n | room of artemis | There are nine women in this forested room holding bows and glaring at you menacingly. | bow🏹") 
+print("s") # inside room of artemis
+print("spawn")
+print("snake🐍")
+print("dig s n | room of rejuvenation | You are in peaceful room to rest. There are jarring artificial lights and nurses talking about your many symptoms. Ok, maybe not so peaceful. | hospital🏥") 
+print("s") # inside the room of rejuvenation
+print("spawn")
+print("vampire")
+print("dig s n | room of strength | Just by standing in this room you feel stronger, better, faster. Beats self-help books, eh? | the-world🌍") 
+print("s") # inside the room of strength
+print("spawn")
+print("god-of-north-wind🌬")
+print("dig s n | room of vesuvius | Sure, walk into the room with an erupting volcano. That was a great idea. Let us add it to the sequence of your other brilliant life choices. | volcanic-mountain🌋") 
+print("s") # inside the room of vesuvius
+print("spawn")
+print("fire🔥")
+print("n") # back inside the room of strength
+print("n") # back inside the room of rejuvenation
+print("n") # back inside the room of artemis
+print("n") # back inside the room of slime
+print("n") # back inside the room of the page of swords 
+print("dig w e | room of powerful tea | A club of tea drinkers stares at you when you walk in. You've disturbed their tea hour with your presence. Maybe the coffee club has openings. Clearly, the tea group wants nothing to do with you. | tea🍵") 
+print("w") # inside the room of powerful tea
+print("spawn")
+print("bad-weather⛈")
+print("dig s n | home | There's no place like home. Except this dungeon room, which also feels like home. |  grandmas-pie🥧") 
+print("s") # inside home 
+print("spawn")
+print("monarch-butterfly🦋")
+print("dig s n | room of the cute I | This room is unbearably cute. You need to get out, as soon as possible. | tulip🌷") 
+print("s") # inside room of the cute I 
+print("spawn")
+print("octopus🐙")
+print("dig s n | room of the cute II | It's hard to determine if this room is in fact cuter than the previous cute room, which was also super cute. | flower🌸") 
+print("s") # inside room of the cute II 
+print("spawn")
+print("blowfish🐡")
+print("dig s n | room of the pacific | You are in a room with water up to your knees.  At least it is not the Arctic Ocean. | shell🐚") 
+print("s") # inside room of the pacific
+print("spawn")
+print("whale🐳")
+print("dig e w | room of the sea floor | The ground is covered with nefarious shrimp and sharp coral. How pleasant. | paradise-island🏝") 
+print("e") # inside room of the sea floor
+print("spawn")
+print("evil-shrimp🦐") 
+print("dig e w | room of the hungry | You have been in this dungeon for a long time now. Face it. You are hungry. Maybe you can manifest some food. | corn🌽") 
+print("e") # inside room of the hungry
+print("spawn")
+print("taco🌮")
+print("w") # back inside room of the sea floor
+print("w") # back inside room of the pacific 
+print("n") # back inside room of the cute II
+print("n") # back inside room of the cute I
+print("n") # back inside home
+print("n") # back inside room of powerful tea
+print("e") # back inside room of the page of swords 
+print("n") # back inside room of the perpetually lost
 print("n")
 print("n") 
 print("n")
